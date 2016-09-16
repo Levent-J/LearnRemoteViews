d7218ae82bcaa90495480640c51080e15ef75549
Levent_J <liwenjing960110@gmail.com>
2016-09-16 22:53:17 +0800

    普通的notification(原来api11之后是这样创建通知的，囧)

--- app/src/main/java/com/levent_j/learnremoteviews/MainActivity.java
+++ app/src/main/java/com/levent_j/learnremoteviews/MainActivity.java
@@ -1,13 +1,48 @@
 package com.levent_j.learnremoteviews;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btn_notify);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_notify:
+                Intent intent = new Intent(this,Main2Activity.class);
+                PendingIntent pendingIntent=PendingIntent.getActivities(this,0, new Intent[]{intent},PendingIntent.FLAG_UPDATE_CURRENT);
+                Notification notification = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    notification = new Notification.Builder(this)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setTicker("有消息了!")
+                            .setContentTitle("标题")
+                            .setContentText("内容")
+                            .setContentIntent(pendingIntent)
+                            .setNumber(1)
+                            .build();
+                }
+                notification.flags=Notification.FLAG_AUTO_CANCEL;
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(1,notification);
+
+                break;
+        }
     }
 }
